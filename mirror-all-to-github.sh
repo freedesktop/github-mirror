b07efb1c5d655a2c06598c1909ed6a7dc27a8948
Ray Strode <rstrode@redhat.com>
2016-12-06 03:10:54 +0000

    post-receive-mirror-github: rename to sync-github-mirror
    it's not longer a post-receive hook, so it's weird to call it one.

--- mirror-all-to-github.sh
+++ mirror-all-to-github.sh
@@ -5,7 +5,7 @@ find  -path './users' -prune -o -name config -print | while read repo_config; do
     pushd $(dirname $repo_config) 
 
     if [ -e git-daemon-export-ok -a -e HEAD -a -e $(awk '{ print $2 }' HEAD) ]; then
-        /git/bin/post-receive-mirror-github
+        /git/bin/sync-github-mirror
     fi
 
     popd
