a31858c339d0c41273fc078f89d81fdcdfbcc807
Ray Strode <rstrode@redhat.com>
2016-12-07 19:23:10 +0000

    new-git-repo.sh: mv update script to final name
    it has the .sample extension by default these days.

--- new-git-repo.sh
+++ new-git-repo.sh
@@ -10,6 +10,7 @@ GIT_DIR="$1" git init-db --shared
 chgrp -R $2 "$1"
 echo "$3" > "$1"/description
 touch "$1"/git-daemon-export-ok
+mv "$1"/hooks/update.sample "$1"/hooks/update
 chmod +x "$1"/hooks/update
 
 bin/set-up-post-receive-hooks.sh "$1"
